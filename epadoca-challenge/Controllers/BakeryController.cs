0fbe46e8d2feb07df81d50a7472e386b93cd85b4
Flavio <fjcf@poli.br>
2023-01-29 15:57:36 -0300

    feat: create footer and whatsapp floating

---
+++ epadoca-challenge/Controllers/BakeryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace epadoca_challenge.Controllers
+{
+    public class BakeryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
